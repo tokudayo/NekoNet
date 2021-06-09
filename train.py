59c513b72639aeed246e1e93b6ef0e1c6b126731
Tô Đức <20toduc01@gmail.com>
2021-06-09 21:24:19 +0700

    Moved DataPipeline to utils.py

--- train.py
+++ train.py
@@ -1,45 +1,8 @@
-import torch, os
-import numpy as np
+import torch
 from model import Net
-from torchvision.io import read_image
+from utils import DataPipeline
 import torchvision.transforms as T
 from triplet_loss import TripletLoss
-
-class DataPipeline():
-    def __init__(self, path, bs, tsnf=None, device='cpu'):
-        self.path = path
-        self.bs = bs
-        self.tsnf = tsnf
-        self.class_data = []
-        self.num_class = len(os.listdir(path))
-        for c in range(self.num_class):
-            class_path = path + '/' + str(c)
-            self.class_data.append(os.listdir(class_path))
-            
-    def example_from_class(self, c):
-        return np.random.choice(self.class_data[c])
-
-    def class_size(self, c):
-        return len(self.class_data[c])
-
-    def get(self):
-        priority = np.random.permutation(self.num_class)
-        batchX = []
-        batchY = []
-        for c in priority:
-            class_path = self.path + '/' + str(c)
-            for imname in self.class_data[c]:
-                if len(batchX) == self.bs: break
-                img = read_image(class_path + '/' + imname)
-                img = self.tsnf(img)
-                batchX.append(img)
-                batchY.append(c)
-            if len(batchX) == self.bs: break
-        return torch.stack(batchX), torch.Tensor(batchY)
-
-
-
-from model import Net
 
 dl = DataPipeline('./data', 16, tsnf = T.Compose([T.Resize((224, 224)),
                                                   lambda x : x/255.0,
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from torchvision.io import read_image
+
 def params_info(net):
     total = 0
     trainable = 0
@@ -9,3 +11,36 @@ def params_info(net):
     print(f'Total parameters: {total}')
     print(f'Trainable: {trainable}')
     print(f'Non-trainable: {total - trainable}')
+
+
+class DataPipeline():
+    def __init__(self, path, bs, tsnf=None, device='cpu'):
+        self.path = path
+        self.bs = bs
+        self.tsnf = tsnf
+        self.class_data = []
+        self.num_class = len(os.listdir(path))
+        for c in range(self.num_class):
+            class_path = path + '/' + str(c)
+            self.class_data.append(os.listdir(class_path))
+            
+    def example_from_class(self, c):
+        return np.random.choice(self.class_data[c])
+
+    def class_size(self, c):
+        return len(self.class_data[c])
+
+    def get(self):
+        priority = np.random.permutation(self.num_class)
+        batchX = []
+        batchY = []
+        for c in priority:
+            class_path = self.path + '/' + str(c)
+            for imname in self.class_data[c]:
+                if len(batchX) == self.bs: break
+                img = read_image(class_path + '/' + imname)
+                img = self.tsnf(img)
+                batchX.append(img)
+                batchY.append(c)
+            if len(batchX) == self.bs: break
+        return torch.stack(batchX), torch.Tensor(batchY)
