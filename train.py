15b42c2087a26fd24b5cf0526b3d21b62316731a
Tô Đức <20toduc01@gmail.com>
2021-06-09 11:22:42 +0700

    Dataloader checkpoint

--- train.py
+++ train.py
@@ -6,9 +6,10 @@ from torchvision import transforms, utils
 
 
 class DataPipeline():
-    def __init__(self, path, bs):
+    def __init__(self, path, bs, tsnf):
         self.path = path
         self.bs = bs
+        self.tsnf = tsnf
         self.class_data = []
         self.num_class = len(os.listdir(path))
         for c in range(self.num_class):
@@ -19,3 +20,15 @@ class DataPipeline():
     def example_from_class(self, c):
         return np.random.choice(self.class_data[c])
 
+    def class_size(self, c):
+        return len(self.class_data[c])
+
+    def get(self):
+        priority = np.random.permutation(self.num_class)
+        class_pointer = 0
+        batch = []
+        for c in priority:
+            if len(batch) + self.class_size(c) > self.bs:
+                # random selection
+                pass
+            batch.append()
