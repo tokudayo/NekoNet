309165f44a2287ad5dac6fddc588e4b0b021f413
Tô Đức <20toduc01@gmail.com>
2021-06-11 02:06:12 +0700

    A proper dataloader

--- utils.py
+++ utils.py
@@ -25,24 +25,34 @@ class DataPipeline():
         for c in range(self.num_class):
             class_path = path + '/' + str(c)
             self.class_data.append(os.listdir(class_path))
-            
+
+    def __iter__(self):
+        self.priority = np.random.permutation(self.num_class)
+        self.ptr = 0
+        self.full_loop = False
+        return self
+    
+    def __next__(self):
+        while self.full_loop == False:
+            batchX = []
+            batchY = []
+            while len(batchX) < self.bs:
+                c = self.priority[self.ptr]
+                class_path = self.path + '/' + str(c)
+                for imname in self.class_data[c]:
+                    if len(batchX) == self.bs: break
+                    img = read_image(class_path + '/' + imname)
+                    img = self.tsnf(img)
+                    batchX.append(img)
+                    batchY.append(c)
+                self.ptr += 1
+                if self.ptr == self.num_class:
+                    self.ptr = 0
+                    self.full_loop = True
+            yield torch.stack(batchX), torch.Tensor(batchY)
+
     def example_from_class(self, c):
         return np.random.choice(self.class_data[c])
 
     def class_size(self, c):
         return len(self.class_data[c])
-
-    def get(self):
-        priority = np.random.permutation(self.num_class)
-        batchX = []
-        batchY = []
-        for c in priority:
-            class_path = self.path + '/' + str(c)
-            for imname in self.class_data[c]:
-                if len(batchX) == self.bs: break
-                img = read_image(class_path + '/' + imname)
-                img = self.tsnf(img)
-                batchX.append(img)
-                batchY.append(c)
-            if len(batchX) == self.bs: break
-        return torch.stack(batchX), torch.Tensor(batchY)
