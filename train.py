953424494388e4261c660ae213080ca865d86d7b
Tô Đức <20toduc01@gmail.com>
2021-06-13 10:27:44 +0700

    Tweaked train function

--- train.py
+++ train.py
@@ -4,24 +4,32 @@ from utils import DataPipeline
 import torchvision.transforms as T
 from triplet_loss import TripletLoss
 
-dl = DataPipeline('./data', 16, tsnf = T.Compose([T.Resize((224, 224)),
-                                                  lambda x : x/255.0,
-                                                  T.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])]))
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+epoch = 20
+batch_size = 32
+
+transform = T.Compose([T.Resize((224, 224)),
+                       lambda x : x/255.0,
+                       T.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
+loader = DataPipeline('./data', batch_size, tsnf = transform)
 
 model = Net()
-criterion = TripletLoss('cpu')
+model = model.to(device)
+criterion = TripletLoss(device)
 optimizer = torch.optim.Adam(model.parameters())
 
-for i in range(20):
-    print(f"Batch {i + 1}")
-    x, y = dl.get()
-    print("FF")
-    # FF
-    embeddings = model(x)
-    loss = criterion(embeddings, y)
-    print(f"loss = {loss.item()}")
-    print("BP")
-    # BP
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+for ep in range(epoch):
+    print(f'Epoch {ep + 1}')
+    for x, y in loader.generator():
+        x = x.to(device)
+        y = y.to(device)
+        print("FF")
+        # FF
+        embeddings = model(x)
+        loss = criterion(embeddings, y)
+        print(f"loss = {loss.item()}")
+        print("BP")
+        # BP
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
