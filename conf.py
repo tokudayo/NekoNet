54b0c405d8acf46e39fe4323c321f684c90ef862
Tô Đức <20toduc01@gmail.com>
2021-06-21 08:38:31 +0700

    Moved some training configuration to a seperate file

---
+++ conf.py
@@ -0,0 +1,8 @@
+# Some training configuration
+epochs = 50
+batch_size = 16
+val_step = 3
+train_path = './data/train'
+val_path = None
+test_path = None
+out_dir = './exp/samplegor3'
--- train.py
+++ train.py
@@ -1,29 +1,21 @@
 import torch, os
-from torchvision.transforms import transforms
-from model import *
-from dataloader import DataLoader
-from utils import *
-import torchvision.transforms as T
-from loss import TripletLossWithGOR
 from matplotlib import pyplot as plt
+from model import *
+from utils import *
 
-# Model and training configuration
+from dataloader import DataLoader
+from loss import TripletLossWithGOR
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-epochs = 50
-batch_size = 16
-val_step = 3
-out_dir = './exp/samplegor2'
 
-transform = T.Compose([T.Resize((224, 224)),
-                       T.transforms.ColorJitter(brightness = .5, contrast = 0.3),
-                       lambda x : x/255.0,
-                       T.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
-loader = DataLoader('./data/train', batch_size, tsnf = transform)
-
+# Configuration
+from conf import *
 model = MobileNetV3L64()
 model = model.to(device)
 criterion = TripletLossWithGOR(device)
 optimizer = torch.optim.Adam(model.parameters())
+
+loader = DataLoader(train_path, batch_size, tsnf = model.transform)
 
 # Training
 ## Continue/start new training
@@ -35,14 +27,14 @@ if os.path.isfile(out_dir + '/last.pt'):
     state = torch.load(out_dir + '/last.pt')
     model.load_state_dict(state['state_dict'])
     optimizer.load_state_dict(state['optimizer'])
-    current = state['epoch']
+    current = state['epoch'] + 1
     print(f'Resume training at epoch {current + 1}')
 else:
     current = 0
 
 loss_epoch = []
 ## Training loop
-for ep in range(current + 1, epochs + 1):
+for ep in range(current, epochs):
     # Processing per epoch
     loss_batch = []
     print(f'Epoch {ep + 1}')
